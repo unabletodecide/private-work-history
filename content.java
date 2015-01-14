d57b5e221f840fed59e2b0a4ff426d10e403d4f5
vikiroot <vivek@botzee.com>
2015-01-14 18:40:15 +0000

    add code in files types: .h,.html,.git-id,.properties,.java,.scala remove code in files types: .properties,.java,.scala

--- content.java
+++ content.java
@@ -14,6 +14,6 @@ public class Cwlcye {
         System.out.println("sbskt");
         System.out.println("vlpsr");
         System.out.println("lgnjn");
-        System.out.println("opxkx");
+        System.out.println("gwvuf");
     }
 }
