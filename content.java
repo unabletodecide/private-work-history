a4fea959511ce8dc1499323aa0bef04946578c8b
vikiroot <vivek@botzee.com>
2015-06-25 02:09:48 +0000

    add code in files types: .java,.scala,.conf remove code in files types: .java,.scala

--- content.java
+++ content.java
@@ -14,6 +14,6 @@ public class Cwlcye {
         System.out.println("sbskt");
         System.out.println("vlpsr");
         System.out.println("jaklq");
-        System.out.println("blbfr");
+        System.out.println("jwcxj");
     }
 }
