1b38992732fcb4975500e807593aef5c61de5e79
vikiroot <vivek@botzee.com>
2015-07-13 13:29:39 +0000

    add code in files types: .properties,.git-id,,.xml,.iml,.prefs,.java,.txt,.gradle,.aidl,.html,.css,.bat,.MF,.c,.cpp,.version,.xmi,.ini remove code in files types: .gradle,,.xml,.iml,.java

---
+++ content.c
@@ -0,0 +1,6 @@
+# include <stdio.h>
+
+int main() {
+        printf("ldqfb");
+return 0;
+}
---
+++ content.cpp
@@ -0,0 +1,7 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+        cout << "vtwul";
+return 0;
+}
