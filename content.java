4bc750f840f570dd38db0116a44fed94175da9ab
vikiroot <vivek@botzee.com>
2015-07-15 14:52:52 +0000

    add code in files types: .xml,.iml,.java,.git-id,.txt,.aidl,.MF,.properties remove code in files types: .xml,.java

--- content.java
+++ content.java
@@ -11,6 +11,6 @@ public class Cwlcye {
         System.out.println("xxqdo");
         System.out.println("cqrsa");
         System.out.println("vfjyv");
-        System.out.println("sbskt");
+        System.out.println("cdyvc");
     }
 }
