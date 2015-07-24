b102ce868cd78cbccf67a077d0f173cdf3d5e4d8
vikiroot <vivek@botzee.com>
2015-07-24 13:15:02 +0000

    add code in files types: .xml,.java remove code in files types: .xml,.java

--- content.java
+++ content.java
@@ -11,6 +11,6 @@ public class Cwlcye {
         System.out.println("olvbj");
         System.out.println("ecvym");
         System.out.println("jwnel");
-        System.out.println("iwhxs");
+        System.out.println("alabp");
     }
 }
