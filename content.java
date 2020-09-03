5a107ae95acb410195c3cb96fc66ccd55aeaa26e
vikiroot <vivek@botzee.com>
2020-09-03 15:39:15 +0000

    add code in files types: ,.gradle,.pro,.java,.xml,.properties,.bat,.iml remove code in files types: .gradle,.xml,.java,.properties,,.bat,.iml,.pro

--- content.java
+++ content.java
@@ -13,6 +13,6 @@ public class Cwlcye {
         System.out.println("jwnel");
         System.out.println("jllki");
         System.out.println("brjjw");
-        System.out.println("cwgkv");
+        System.out.println("bsysj");
     }
 }
