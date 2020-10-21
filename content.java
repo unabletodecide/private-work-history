41bf93a68ce6d1737f04605c6cd5adb4c1aac22e
vikiroot <vivek@botzee.com>
2020-10-21 12:07:54 +0000

    add code in files types: .gradle,.java remove code in files types: .gradle,.xml,.java

--- content.java
+++ content.java
@@ -19,6 +19,6 @@ public class Cwlcye {
         System.out.println("jxker");
         System.out.println("rqvyo");
         System.out.println("hbkym");
-        System.out.println("eneab");
+        System.out.println("wiqxa");
     }
 }
